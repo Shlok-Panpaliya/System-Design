96878f19ad15e381000b3af8431dce3c780b003f
Shlok-Panpaliya <shlokp762@gmail.com>
2020-09-26 11:54:35 +0530

    Working fine final

--- consistent-hashing/consistent_hashing.py
+++ consistent-hashing/consistent_hashing.py
@@ -6,16 +6,17 @@ class HashRing(object):
     #nodes represent list of object(mem_chache server) that have string representation
     #repicas represent how many virtual server should be there for even distribution
 
-    self.replica = replica
-    self.ring = dict()
-    self.sorted_keys = []
-    if nodes:
-        for nodes in nodes:
-            self.add_node(node)
-
+        self.replica = replica
+        self.ring = dict()
+        self.sorted_keys = []
+        if nodes:
+            for node in nodes:
+                self.add_node(node)
+     #   print(self.ring)
+      #  print(self.sorted_keys)
     def add_node(self,node):
         # Adds the node to the hash ring with given number of replicas
-        for i in range(self.replicas):
+        for i in range(self.replica):
             key = self.gen_key('%s:%s' % (node, i))
             self.ring[key] = node
             self.sorted_keys.append(key)
@@ -27,12 +28,12 @@ class HashRing(object):
         m = md5()
         key = key.encode('utf-16')
         m.update(key)
-        return m.hexdigest(key,16) 
+        return (m.hexdigest(),16) 
 
     def get_node(self,string_key):
         #It returns a corresponding node(server) to the string_key
 
-        return self.get_nodes(self,string_key)
+        return self.get_node_pos(string_key)
 
     def get_node_pos(self,string_key):
 
@@ -45,6 +46,23 @@ class HashRing(object):
 
         for i in range(len(nodes)):
             if key <= nodes[i]:       #<= because if a server is removed it will find nearest available server
-                return self.ring(nodes[i]),i  #here nodes[i] is the server and i is its position
+                return self.ring[nodes[i]],i  #here nodes[i] is the server and i is its position
         return self.ring(nodes[0]),0
 
+    def remove_node(self,node):
+        #Removes the server and its replicas from the hash ring
+        for i in range(self.replicas):
+
+            key = self.gen_key('%s:%s' % (node, i))
+            del self.ring[key]
+            self.sorted_keys.remove(key)
+
+if __name__ == "__main__":
+    memcache_servers = ['192.168.0.246:11212',
+                    '192.168.0.247:11212',
+                    '192.168.0.249:11212']
+    ring = HashRing(memcache_servers)
+   # print(sorted_keys)
+    s = input("Enter Request id")
+    server = ring.get_node(s)
+    print("server alloted",server[0],"is at position ",server[1])
